59b11e24ec45749e1f99900e3168bd0316dcf829
Aleksandra Yovkova <alex.yovkova@gmail.com>
2020-06-08 23:37:44 +0300

    Handle uploading only non-existing images

--- server/upload.php
+++ server/upload.php
@@ -19,21 +19,37 @@ if (isset($_POST['submit'])) {
             // TODO: The following steps should be implemented right after we understand how to parse the metadata and hash the image content
             // NOTE: Leave the steps from this comment after implementing this
             // 1. Hash the image content
-            // 2. Check whether the hash is present on the server. For the check -> we have a JSON file storing the info in
+            $hash = hash_file('sha256', $fileTmpName);
+
+            // 2. Check whether the hash is present on the server. For the check -> we have an image_map.json file storing the info in
             //    { image_hash: path_to_image, ...} format
+            $image_map = file_get_contents('./images/image_map.json');
+            $json_assoc = json_decode($image_map, true);
+
             // 3.1. If image content is not present, upload the file to the server and take the path from there
             //      Parse the meta data so that we can give the information to the Image model
             //      Create an Image and ImageInstance with the expected data
+            if (!isset($json_assoc[$hash])) {
+                // Upload the file to server
+                $filenameNew = uniqid($realFilename.'_').'.'.$fileExt;
+                $fileDestination = 'images/'.$filenameNew;
+
+                move_uploaded_file($fileTmpName, $fileDestination);
+
+                // Append {hash: path_to_image} to the JSON
+                $json_assoc[$hash] = $filenameNew;
+                $json_data = json_encode($json_assoc);
+                file_put_contents('./images/image_map.json', $json_data);
+            }
+
             // 3.2. If image content is present, take the image path from the JSON by the hash key
             //      Get the Image instance which has this path. Create new ImageInstance with FK to this Image. 
             //      Increase number_instances of the Image
             
+            else {
+                $fileDestination = $json_assoc[$hash];
+            }
             
-            // Upload the file to server
-            $filenameNew = uniqid($realFilename.'_').'.'.$fileExt;
-            $fileDestination = './images/'.$filenameNew;
-
-            move_uploaded_file($fileTmpName, $fileDestination);
             header("Location: ../client/index.php?success=uploaded");
 
         } else {
