21d4e29685f8464bbd8d57ba5d2f70d0f694a9de
Elena Tuparova <47596711+elenatuparova@users.noreply.github.com>
2020-06-11 19:05:10 +0300

    Restrict non-logged in users from uploading and try to add 'on this day x years ago'

--- client/index.php
+++ client/index.php
@@ -5,20 +5,52 @@
 <?php
     if (isset($_GET['success'])){
         if ($_GET['success'] == 'register') {
-            echo '<div class="success-msg">You have successfully created your profile.</div>';
+            echo '<div class="success-msg">You have successfully created your profile. Please login.</div>';
         } else if ($_GET['success'] == 'uploaded') {
             echo '<div class="success-msg">You have successfully uploaded your image.</div>';
         }
     }
+
+    echo '<main class="container">';
+    require "../server/dbhandler.php";
+    if (isset($_SESSION['userId'])) {
+        echo '<form action="../server/upload.php" method="POST" enctype="multipart/form-data">
+                  <input type="file" name="file">
+                  <div>
+                    <button type="submit" name="submit" class="form-button">Upload your image</button>
+                  </div>
+              </form>';
+        for ($i = 0; $i <= 10; $i++) {
+            $todayYearsAgo = date("Y-m-d", strtotime("-$i years"));
+            $query = "SELECT path FROM images WHERE {
+                        id IN (SELECT image_id FROM image_instances WHERE user_id=?
+                        AND timestamp BETWEEN '? 00:00:00' AND '? 23:59:59'";
+            $statement = mysqli_stmt_init($conn);
+
+            if (!mysqli_stmt_prepare($statement, $query)) {
+                header("Location: index.php?error=sqlerror");
+                exit();
+            }
+            else {
+                mysqli_stmt_bind_param($statement, "iss", $_SESSION['userId'], $todayYearsAgo, $todayYearsAgo);
+                mysqli_stmt_execute($statement);
+                $result = mysqli_stmt_get_result($statement);
+                if (mysqli_num_rows($result) > 0) {
+                    echo "<h1>You have memories from this day $todayYearsAgo years ago. Check them out:<h1>";
+                    while($row = mysqli_fetch_assoc($result)){
+                        echo '<span><img width="30%" style="margin: 1%;" src="../server/images/'.$row['path'].'"></span>';
+                    }
+                }
+
+            }
+        }
+    }
+    else {
+        echo '<p>Welcome to Image Gallery! Please log into your profile or register to be able to upload pictures and
+         generate galleries!</p>';
+    }
+    echo '</main>';
 ?>
-<main class="container">
-    <form action="../server/upload.php" method="POST" enctype="multipart/form-data">
-        <input type="file" name="file">
-        <div>
-            <button type="submit" name="submit" class="form-button">Upload your image</button>
-        </div>
-    </form>
-</main>
 
 <?php 
     require "footer.php";
